25ed2a350d2c8ce2074d3b7c11daca26d5d98d17
pierreG-dev <pierre.godino@hotmail.com>
2021-11-23 10:33:26 +0100

    2e exercice

---
+++ EX2.js
@@ -0,0 +1,7 @@
+let a = Math.floor(Math.random() * 100)
+let b = Math.floor(Math.random() * 100)
+
+let somme = a + b
+let phrase_finale = a + " + " + b + " = " + somme
+
+console.log(phrase_finale)
